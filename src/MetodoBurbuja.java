c830eea5faacd8f39f6c58425399b13c35cbd671
unknown <mayancelaevelyn8@.com>
2026-04-21 18:21:31 -0500

    proyecto actualizado

--- src/MetodoBurbuja.java
+++ src/MetodoBurbuja.java
@@ -15,7 +15,7 @@ public class MetodoBurbuja {
     public void ordenarAscendente(){
         for(int i=0; i<arreglo.length;i++){
             for(int j = i+1; j<arreglo.length;j++){
-                if(arreglo[i] > arreglo[j]){
+                if(arreglo[i] < arreglo[j]){
                     int aux = arreglo[i];
                     arreglo[i] = arreglo[j];
                     arreglo[j] = aux;
